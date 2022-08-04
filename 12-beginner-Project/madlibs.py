c046ca2dd4c4dcd12ac663517a3e45159a712533
nadia-iqbal <61611610+nadia-iqbal@users.noreply.github.com>
2022-08-04 17:08:53 +0600

    Create madlibs.py

---
+++ 12-beginner-Project/madlibs.py
@@ -0,0 +1,9 @@
+#string concatenation (how to put string together)
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("verb: ")
+madlibs = f"Computer programming is so {adj}. It makes me so excited all the time because I love to {verb1}." \
+          f"Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlibs)
